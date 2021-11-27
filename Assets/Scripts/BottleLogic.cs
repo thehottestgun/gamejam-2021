570d25bc2987678617bfdacc47950964c10589c2
Karolina Struzek <s20686@pjwstk.edu.pl>
2021-11-27 18:43:57 +0100

    upp

---
+++ Assets/Scripts/BottleLogic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleLogic : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerStats.playerHp--;
+            UIUpdate.instance.SetHp(PlayerStats.playerHp);
+            Destroy(gameObject);
+        }
+    }
+}
