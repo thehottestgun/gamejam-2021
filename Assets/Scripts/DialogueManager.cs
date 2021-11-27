e8c794842549ee12baa796ba9aec81e5195a7060
Kamil Barczak <kamil-barczak@outlook.com>
2021-11-27 17:12:37 +0100

    dialogue progress

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,55 @@ using UnityEngine;
 public class DialogueManager : MonoBehaviour
 {
     private Queue<string> _sentences;
+
+    private bool _inDialogue;
     // Start is called before the first frame update
     void Start()
     {
         _sentences = new Queue<string>();
+        _inDialogue = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Confirm") && _inDialogue)
+        {
+            DisplayNextSentence();
+        }
     }
 
     public void StartDialogue(Dialogue dialogue)
     {
         Debug.Log("Starting conversation with " + dialogue.name);
+
+        _inDialogue = true;
+        
+        _sentences.Clear();
+
+        foreach (var sentence in dialogue.sentences)
+        {
+            _sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (_sentences.Count == 0)
+        {
+            _inDialogue = false;
+            EndDialogue();
+            return;
+        }
+
+        var sentence = _sentences.Dequeue();
+        
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation.");
     }
 }
