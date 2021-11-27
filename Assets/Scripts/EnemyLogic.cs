f4bdf1fd75b2200d965d5aa3e83039525a6318ad
s20524 <60384828+s20524@users.noreply.github.com>
2021-11-27 23:18:00 +0100

    fd

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -11,17 +11,22 @@ public class EnemyLogic : MonoBehaviour
     private Transform _enemy;
     private Rigidbody2D _rb;
     private SpriteRenderer _sr;
+
+    private Animator _an;
     //private bool _inAir;
     public int jumpForce;
     public int speed;
     public int enemyRangeX;
     public int enemyRangeY;
     
+    
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player").transform;
         _sr = GetComponent<SpriteRenderer>();
+        _an = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody2D>();
+        
         //_inAir = false;
         _enemy = gameObject.transform;
         
@@ -37,16 +42,17 @@ public class EnemyLogic : MonoBehaviour
     {
         if (Math.Abs(_player.position.x - _enemy.position.x) < enemyRangeX || Math.Abs(_player.position.y - _enemy.position.y) < enemyRangeY)
         {
+            _an.SetFloat("Speed",speed);
             Debug.Log("Enemy Chase");
             if (_player.position.x > _enemy.position.x)
             {
                 _enemy.Translate(new Vector2(speed*Time.deltaTime,0));
-                _sr.flipX = false;
+                _sr.flipX = true;
             }
             else
             {
                 _enemy.Translate(new Vector2(-speed*Time.deltaTime,0));
-                _sr.flipX = true;
+                _sr.flipX = false;
             }
         }
     }
@@ -54,6 +60,7 @@ public class EnemyLogic : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            _an.SetBool("CanAttack",true);
             Debug.Log("Player Get Damage");
         }
 
@@ -61,5 +68,14 @@ public class EnemyLogic : MonoBehaviour
         {
             //_inAir = false;
         }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _an.SetBool("CanAttack",false);
+        }
+
     }
 }
