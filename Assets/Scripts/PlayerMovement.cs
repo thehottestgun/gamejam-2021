511f10e6fde4584b642f0a9f5ba27caac62ac5f5
Kamil Barczak <kamil-barczak@outlook.com>
2021-11-27 13:48:58 +0100

    Scripted PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed = 3.0f, jumpForce = 5.0f;
+    private Rigidbody2D _rigidbody;
+
+    private bool _canJump = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        var xDisplacement = Input.GetAxis("Horizontal");
+        Debug.Log(xDisplacement);
+        var yDisplacement = Input.GetAxis("Vertical");
+        _rigidbody.velocity = new Vector2(xDisplacement * speed, _rigidbody.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (!Input.GetButton("Jump") || !_canJump) return;
+        Debug.Log("Jumping");
+        _canJump = false;
+        _rigidbody.AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            _canJump = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            _canJump = false;
+        }
     }
 }
