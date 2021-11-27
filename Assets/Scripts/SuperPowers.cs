c8f8df34bc4f2eea236c14262a160fc87496b68e
s20524 <60384828+s20524@users.noreply.github.com>
2021-11-27 18:23:30 +0100

    x

--- Assets/Scripts/SuperPowers.cs
+++ Assets/Scripts/SuperPowers.cs
@@ -8,19 +8,21 @@ public class SuperPowers : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private PostProcessVolume _postProcessVolume;
     private Vignette _vignette;
-    private bool _toogle;
-    
-    
+    public float vignetteSpeed;
+    private int _toogle;
+  
+
+
     void Start()
     {
         _postProcessVolume.profile.TryGetSettings(out _vignette);
-        _toogle = true;
-
+        _toogle = 0;
     }
     
     // Update is called once per frame
     void Update()
     {
+        Invisible();
         if(PlayerStats.superPower==1) // Level 1
             Invisible();
         if(PlayerStats.superPower==2) // Level 2
@@ -31,17 +33,43 @@ public class SuperPowers : MonoBehaviour
     {
         if (Input.GetButtonDown("SuperPower"))
         {
-            if (_toogle)
+            if (_toogle==0)
             {
-                _vignette.intensity.value = 1; 
-                _toogle = false;
+                Debug.Log("Start U");
+                StartCoroutine(ChangeUp());
             }
-            else
+            if(_toogle==1)
             {
-                _vignette.intensity.value = 0; 
-                _toogle = true;
+                Debug.Log("Start D");
+                StartCoroutine(ChangeDown());
             }
         }
+    }
+
+    public IEnumerator ChangeUp()
+    {
+        _toogle = -1;
+        while (_vignette.intensity.value<=1)
+        { 
+            _vignette.intensity.value += vignetteSpeed; 
+            yield return new WaitForSeconds(0.1f);
+        }
+        _toogle = 1;
+        StopCoroutine(ChangeUp());
+            
+    }
+
+    public IEnumerator ChangeDown()
+    {
+        _toogle = -1;
+        while (_vignette.intensity.value>=0)
+        { 
+            _vignette.intensity.value -= vignetteSpeed; 
+            yield return new WaitForSeconds(0.1f);
+        }
+        _toogle = 0;
+        StopCoroutine(ChangeDown());
+            
     }
 
     private void Teleportation()
