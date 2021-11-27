cf1857882276bb29f83272e1a88d13bf74c5a3e1
Kamil Barczak <kamil-barczak@outlook.com>
2021-11-27 14:33:40 +0100

    Changed Interaction to Abstract class, created Door Class for doors specifically

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interaction : MonoBehaviour
+public abstract class Interaction : MonoBehaviour
 {
     private bool _inRange;
     // Start is called before the first frame update
