9775467113e8862af19aa836a92afc76ea0bea88
Kamil Barczak <kamil-barczak@outlook.com>
2021-11-27 22:32:42 +0100

    lots of changes

--- Assets/Scripts/CanPoints.cs
+++ Assets/Scripts/CanPoints.cs
@@ -1,16 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CanPoints : MonoBehaviour
 {
+    [SerializeField] private AudioClip _audio;
+
+    private void Start()
+    {
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        {
+        { 
             PlayerStats.cans++;
-            UIUpdate.instance.SetCans(PlayerStats.cans);
-            Destroy(gameObject);
+           AudioSource.PlayClipAtPoint(_audio,transform.position,1);
+           
+            
+           UIUpdate.instance.SetCans(PlayerStats.cans);
+           Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -8,6 +8,8 @@ public class DialogueTrigger : Interaction
     public Dialogue Dialogue;
 
     private GameObject _dialogueManager;
+
+    [SerializeField] private AudioClip clip;
     // Start is called before the first frame update
 
     private void Update()
@@ -15,11 +17,22 @@ public class DialogueTrigger : Interaction
         Interact();
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Picked up");
+        if (other.CompareTag("Player") && PlayerStats.cans == 1)
+        {
+            Debug.Log("Conds");
+            FindObjectOfType<DialogueManager>().StartDialogue(Dialogue);
+            AudioSource.PlayClipAtPoint(clip,transform.position,1);
+        }
+    }
+
     protected override void Interact()
     {
         if (!_inRange || !Input.GetButtonDown("Interaction")) return;
         Debug.Log("Oh boy");
         FindObjectOfType<DialogueManager>().StartDialogue(Dialogue);
-
+        
     }
 }
