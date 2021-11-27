a1ed65c97311bb3b3c632aca054f9aad9598d5e6
s20524 <60384828+s20524@users.noreply.github.com>
2021-11-27 15:11:09 +0100

    kappa

--- Assets/Scripts/SuperPowers.cs
+++ Assets/Scripts/SuperPowers.cs
@@ -8,9 +8,13 @@ public class SuperPowers : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private PostProcessVolume _postProcessVolume;
     private Vignette _vignette;
+    private IEnumerator _loop;
+    
     void Start()
     {
         _postProcessVolume.profile.TryGetSettings(out _vignette);
+        _loop = Change();
+        
     }
 
     // Update is called once per frame
@@ -23,7 +27,31 @@ public class SuperPowers : MonoBehaviour
     {
         if (Input.GetButton("Jump"))
         {
-            _vignette.intensity.value = 0;
+            StartCoroutine(_loop);
         }
+    }
+
+    private IEnumerator Change()
+    {
+        float val = _vignette.intensity.value;
+        if (val == 0)
+        {
+            while (_vignette.intensity.value > 0)
+            {
+                _vignette.intensity.value-=0.1f;
+                yield return new WaitForSeconds(0.5f);
+            }
+            
+        }
+        else
+        {
+            while (_vignette.intensity.value < 1)
+            {
+                _vignette.intensity.value+=0.1f;
+                yield return new WaitForSeconds(0.5f);
+            }
+            
+        }
+        
     }
 }
